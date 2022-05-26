bdaef183328b73d7aacde25acff3a217a9e3648a
Scott <sfitzpatrick55@gmail.com>
2022-05-25 20:42:38 -0400

    Create HttpResponseMessage response and grab the content as a string to deserialize as a collection of PeopleViewModel objects"

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -1,5 +1,7 @@
 using System.Net.Http;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using SwapiMVC.Models;
 
 namespace SwapiMVC.Controllers
 {
@@ -12,7 +14,13 @@ namespace SwapiMVC.Controllers
         }
         public async Task<IActionResult> Index(string page)
         {
-            return View();
+            string route = $"people?page={page ?? "1"}"; // No idea what this is
+            HttpResponseMessage response = await _httpClient.GetAsync(route); // Stepping into the _httpClient field and performing Async function using the route created above as a parameter
+
+            var responseString = await response.Content.ReadAsStringAsync(); // Grabbing the content from the HttpResponseMessage response as a string
+            var people = JsonSerializer.Deserialize<ResultsViewModel<PeopleViewModel>>(responseString); // Deserializing this string from JSON to C# object, a ResultsViewModel that contains a collection of PeopleViewModel objects
+
+            return View(people);
         }
     }
 }
