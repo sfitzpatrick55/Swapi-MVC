7c7ba3ff6baaa2dd3ac635f8e5cf3c19cdb2b5cc
Scott <sfitzpatrick55@gmail.com>
2022-05-25 20:02:54 -0400

    Establish private field _httpClient, Add constructor for PeopleController class, Set _httpClient equal to CreateClient method in httpClientFactory

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -1,13 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SwapiMVC.Controllers
 {
     public class PeopleController : Controller
     {
+        private readonly HttpClient _httpClient; // Establishes a private field to go inside the constructor
+        public PeopleController(IHttpClientFactory httpClientFactory) // Constructor for PeopleController class
+        {
+            _httpClient = httpClientFactory.CreateClient("swapi"); // Setting the new private field _httpClient equal to httpClientFactory and invoke the CreateClient method
+        }
         public IActionResult Index()
         {
             return View();
